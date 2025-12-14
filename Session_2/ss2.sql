f67bf740174d6cb977cb09800743e5ce4343a617
Prabhakar_Kumar_Jha <166113256+23bcs12284@users.noreply.github.com>
2025-12-14 08:15:53 +0530

    submit
    This function retrieves the Nth highest salary from the Employee table.

---
+++ Session_2/ss2.sql
@@ -0,0 +1,20 @@
+-- G3_23BCS12284_Prabhakar
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT)
+RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  IF N <= 0 THEN
+      Salary := NULL;
+      RETURN NEXT;
+      RETURN;
+  END IF;
+
+  RETURN QUERY
+  SELECT (
+      SELECT DISTINCT e.salary
+      FROM Employee e
+      ORDER BY e.salary DESC
+      OFFSET N - 1
+      LIMIT 1
+  );
+END;
+$$ LANGUAGE plpgsql;
